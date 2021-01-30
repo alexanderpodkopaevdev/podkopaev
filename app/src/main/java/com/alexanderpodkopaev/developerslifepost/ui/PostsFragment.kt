53f0d125cd395cca2f910729cfefb22632eb8f2e
Alexander Podkopaev <p-alex1990@mail.ru>
2021-01-30 18:47:54 +0500

    Доработан внешний вид, процесс загрузки

--- app/src/main/java/com/alexanderpodkopaev/developerslifepost/ui/PostsFragment.kt
+++ app/src/main/java/com/alexanderpodkopaev/developerslifepost/ui/PostsFragment.kt
@@ -6,14 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.alexanderpodkopaev.developerslifepost.R
-import com.alexanderpodkopaev.developerslifepost.data.PostModel
 import com.alexanderpodkopaev.developerslifepost.data.PostRepository
 import com.alexanderpodkopaev.developerslifepost.data.PostRepositoryImpl
 import com.alexanderpodkopaev.developerslifepost.data.RetrofitModule
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -24,7 +25,7 @@ class PostsFragment : Fragment() {
     private lateinit var ivPostImage: ImageView
     private lateinit var ibBack: ImageButton
     private lateinit var ibNext: ImageButton
-    private var post: PostModel? = null
+    private lateinit var tvDesc: TextView
 
     private lateinit var postRepository: PostRepository
 
@@ -43,10 +44,26 @@ class PostsFragment : Fragment() {
             PostsViewModelFactory(postRepository)
         ).get(PostsViewModel::class.java)
         viewModel.currentPost.observe(viewLifecycleOwner) {
-            Glide.with(requireContext()).load(it.gifURL).into(ivPostImage)
+            Glide.with(requireContext())
+                .load(it.gifURL)
+                .transform(
+                    RoundedCorners(
+                        requireContext().resources.getDimension(
+                            R.dimen.small
+                        ).toInt()
+                    )
+                ).placeholder(R.drawable.progress_animation)
+                .into(ivPostImage)
+            tvDesc.text = it.description
         }
         viewModel.currentPostPosition.observe(viewLifecycleOwner) { currentPosition ->
             ibBack.isEnabled = currentPosition != 0
+        }
+        viewModel.isError.observe(viewLifecycleOwner) { isError ->
+            if (isError) {
+                ivPostImage.setImageResource(R.drawable.ic_launcher_foreground)
+                tvDesc.text = getString(R.string.error_text)
+            }
         }
         ibNext.setOnClickListener {
             viewModel.getNextPost()
@@ -62,6 +79,7 @@ class PostsFragment : Fragment() {
         ivPostImage = view.findViewById(R.id.ivPostImage)
         ibBack = view.findViewById(R.id.ibBack)
         ibNext = view.findViewById(R.id.ibNext)
+        tvDesc = view.findViewById(R.id.tvDesc)
     }
 
     companion object {
